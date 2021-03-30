5569033d2ed550794de63a3c1899e15accd1f1e4
Adil Beisenov <adilbeisenov@gmail.com>
2021-03-30 15:54:36 +0600

    Add

--- Udder/AppDelegate.swift
+++ Udder/AppDelegate.swift
@@ -31,9 +31,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-    override func viewWillAppear(_ animated: Bool) {
-        //
-    }
+//    override func viewWillAppear(_ animated: Bool) {
+//        //
+//    }
 
 }
 
